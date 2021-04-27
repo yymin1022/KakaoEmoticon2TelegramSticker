86e614fea2e01d1ff2df585c2bca8b9925e2b9a5
yymin1022 <yymin1022@gmail.com>
2021-04-27 14:24:20 +0900

    Add Create Emoticon Command with Basic Return

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ apiKeyFile.close()
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
+def createEmoticon(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Create Command")
+
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, World!")
 
+create_handler = MessageHandler(Filters.text, createEmoticon)
 start_handler = CommandHandler('start', start)
+
+dispatcher.add_handler(create_handler)
 dispatcher.add_handler(start_handler)
+
 updater.start_polling()
