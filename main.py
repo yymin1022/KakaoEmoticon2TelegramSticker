a2712aa5d6d9c5da80fa0e83cbfd2fad712f8eda
yymin1022 <yymin1022@gmail.com>
2021-04-27 14:18:24 +0900

    Add Telegram Bot Basic Code

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import telegram
+from telegram.ext import CommandHandler, Dispatcher, MessageHandler, Updater
+
 apiKeyFile = open("/home/server/KakaoEmoticon2TelegramSticker_KEY", 'r')
 TOKEN = apiKeyFile.read().rstrip('\n')
 apiKeyFile.close()
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello, World!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
