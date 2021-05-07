3bf152bc2b1da5be92fdffed00529322e6d5516c
yymin1022 <yymin1022@gmail.com>
2021-05-07 04:22:09 +0000

    Update Debugging Code

--- main.py
+++ main.py
@@ -37,6 +37,7 @@ def createEmoticon(update, context):
     divTitle = divInfo.find("div", class_="info_product")
     context.bot.send_message(chat_id=update.effective_chat.id, text=divTitle.text)
     strTitle = divTitle.find("h3", class_="tit_product")
+    context.bot.send_message(chat_id=update.effective_chat.id, text=strTitle)
 
     divEmoticons = divContent.find("div", class_="area_emoticon")
     listEmoticons = divEmoticons.find("ul")
