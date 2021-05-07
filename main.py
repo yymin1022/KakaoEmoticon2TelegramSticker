35fa2a06010ce08b62950f1cbfbdb2e590daa19b
yymin1022 <yymin1022@gmail.com>
2021-05-07 06:10:08 +0000

    Update Sticker Set Making Code

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def createEmoticon(update, context):
 
         count += 1
     
-    context.bot.create_new_sticker_set(user_id=1234, name="test_sticker", title="Test Sticker", emojis="😀", contains_masks=False, png_sticker=arrEmoticon)
+    context.bot.create_new_sticker_set(user_id=1234, name="test_sticker", title="Test Sticker", emojis="😀", contains_masks=False, png_sticker=open("emoticonTemp/0.png", "rb"))
     context.bot.send_message(chat_id=update.effective_chat.id, text=str(arrEmoticon))
 
 def helpMenu(update, context):
