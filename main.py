05c3475ed9de7c989d6217443cab74b07af8172f
yymin1022 <yymin1022@gmail.com>
2021-05-09 13:46:15 +0000

    Add All Stickers from Kakao to StickerSet

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def createEmoticon(update, context):
 
         if count == 0:
             curTime = datetime.datetime.now().replace(tzinfo=datetime.timezone.utc).timestamp()
-            stickerName = str(curTime) + "_by_KakaoEmoticon2Telegram_bot"
+            stickerName = str(curTime) + "_by_KakaoEmoticon2Telegram_bot".replace(".", "")
             context.bot.send_message(chat_id=update.effective_chat.id, text=stickerName)
             context.bot.create_new_sticker_set(user_id=318996831, 
                                                 name=stickerName,
