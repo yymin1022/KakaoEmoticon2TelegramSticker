70c1a43b0f17768e98592e7f875cc8a187b5d40e
yymin1022 <yymin1022@gmail.com>
2021-05-10 06:50:48 +0000

    Debugging Code

--- main.py
+++ main.py
@@ -32,7 +32,9 @@ def createEmoticon(update, context):
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(executable_path='/home/server/KakaoEmoticon2TelegramSticker/chromedriver', chrome_options=options)
-    driver.get()
+    
+    context.bot.send_message(chat_id=update.effective_chat.id, text=emoticonURL)
+    driver.get(emoticonURL)
 
     context.bot.send_message(chat_id=update.effective_chat.id, text="이모티콘 정보를 불러오는 중입니다.")
 
