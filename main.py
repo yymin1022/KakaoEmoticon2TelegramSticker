c349d2100fe04af52f260c303ae89349ad714c02
yymin1022 <yymin1022@gmail.com>
2022-08-22 03:20:33 +0900

    Update main.py : Cleaned UP

--- main.py
+++ main.py
@@ -15,9 +15,7 @@ import datetime
 import os
 import time
 
-# apiKeyFile = open("/home/server/KakaoEmoticon2TelegramSticker_KEY", 'r')
 TOKEN = os.getenv("TELEGRAM_TOKEN", "NO_TOKEN")
-# apiKeyFile.close()
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
