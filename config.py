2a6e6f7a73ca25328f8d740597d28fd259d7e2ac
yymin1022 <yymin1022@gmail.com>
2025-12-13 19:06:25 +0900

    Update main.py : Improved telegram session setup

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import logging
+import os
 import re
 
 
 EMOTICON_ID_REGEX = re.compile("https://e.kakao.com/t/.+")
 LOG_LEVEL = logging.INFO
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
--- main.py
+++ main.py
@@ -1,26 +1,20 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 
-import os
-
+from config import TELEGRAM_TOKEN
 from message import create_emoticon
 from util import setup_logger
 
 
 def main():
+    if not TELEGRAM_TOKEN:
+        print("Error: Token missing")
+        return
+
     setup_logger()
 
     telegram_application = (
         ApplicationBuilder()
-        .token(os.getenv("TELEGRAM_TOKEN", "NO_TOKEN"))
-        .http_version("2")
-        .read_timeout(600)
-        .get_updates_read_timeout(600)
-        .write_timeout(600)
-        .get_updates_write_timeout(600)
-        .pool_timeout(600)
-        .get_updates_pool_timeout(600)
-        .connect_timeout(600)
-        .get_updates_connect_timeout(600)
+        .token(TELEGRAM_TOKEN)
         .build()
     )
 
